fff3093b0cf17eb56137b8ec47de77079a8d6f0d
noahzhy <noahzhy@gmail.com>
2021-05-20 13:20:54 +0900

    add docs

--- K210/src/fd_cam/main.c
+++ K210/src/fd_cam/main.c
@@ -7,16 +7,17 @@
 #include "gc0328.h"
 static const char *TAG = "camera_init";
 
+
 static void io_mux_init(void) {
     /* Init DVP IO map and function settings */
-    fpioa_set_function(20, FUNC_CMOS_PCLK);
-    fpioa_set_function(22, FUNC_CMOS_HREF);
-    fpioa_set_function(24, FUNC_CMOS_VSYNC);
-    fpioa_set_function(26, FUNC_SCCB_SCLK);
-    fpioa_set_function(28, FUNC_SCCB_SDA);
-    fpioa_set_function(30, FUNC_CMOS_RST);
-    fpioa_set_function(32, FUNC_CMOS_PWDN);
-    fpioa_set_function(34, FUNC_CMOS_XCLK);
+    fpioa_set_function(40, FUNC_SCCB_SDA);
+    fpioa_set_function(41, FUNC_SCCB_SCLK);
+    fpioa_set_function(42, FUNC_CMOS_RST);
+    fpioa_set_function(43, FUNC_CMOS_VSYNC);
+    fpioa_set_function(44, FUNC_CMOS_PWDN);
+    fpioa_set_function(45, FUNC_CMOS_HREF);
+    fpioa_set_function(46, FUNC_CMOS_XCLK);
+    fpioa_set_function(47, FUNC_CMOS_PCLK);
 
     sysctl_set_spi0_dvp_data(1);
 }
@@ -25,10 +26,6 @@ static void io_set_power(void) {
     /* Set dvp and spi pin to 1.8V */
     sysctl_set_power_mode(SYSCTL_POWER_BANK6, SYSCTL_POWER_V18);
     sysctl_set_power_mode(SYSCTL_POWER_BANK7, SYSCTL_POWER_V18);
-
-    // /* Set dvp and spi pin to 1.8V */
-    // sysctl_set_power_mode(SYSCTL_POWER_BANK1, SYSCTL_POWER_V18);
-    // sysctl_set_power_mode(SYSCTL_POWER_BANK2, SYSCTL_POWER_V18);
 }
 
 void camera_init() {
@@ -43,17 +40,24 @@ void camera_init() {
     // dvp_set_output_enable(1, 1);
     dvp_set_image_format(DVP_CFG_YUV_FORMAT);
     dvp_set_image_size(320, 240);
-    cambus_init(8, 2, 41, 40, 0, 0);  // DVP SCL(41) SDA(40) pin ->software i2c
+    // DVP SCL(41) SDA(40) pin ->software i2c
+    cambus_init(8, 2, 41, 40, 0, 0);
 
     gc0328_reset();
     gc0328_init();
 }
 
-int main(void) {
-
-    
-
-
+void init_sensors() {
+    // camera init
     camera_init();
+    // uart init
+    // wifi init
+
+
+}
+
+int main(void) {
+    init_sensors();
+    
     return 0;
 }
