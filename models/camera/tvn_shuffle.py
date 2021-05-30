ba0dda7908e1114fd429bb8724923b9b578ebfc2
noahzhy <noahzhy@gmail.com>
2021-05-31 04:03:46 +0900

    tvn shuffle channel (relu6)

--- models/camera/tvn_shuffle.py
+++ models/camera/tvn_shuffle.py
@@ -10,6 +10,9 @@ from keras.utils.vis_utils import plot_model
 batch_size = 64
 num_frames = 2
 
+block1_conv_filter = 32
+block2_conv_filter = 128
+
 SE_RATIO = 2
 BN_DECAY = 0.9
 BN_EPSILON = 1e-5
@@ -37,18 +40,18 @@ def bn_relu(inputs, relu="relu", init_zero=False):
         x = ReLU()(x)
     elif relu == "relu6":
         x = tf.nn.relu6(x)
-    else:
-        pass
+    # else:
+    #     pass
     return x
 
 
 def Conv_DWConv_Conv(inputs, out_channels, stride=1):
     x = Conv2D(out_channels, kernel_size=1, padding="same")(inputs)
-    x = bn_relu(x, relu="relu")
-    x = DepthwiseConv2D(kernel_size=3, strides=stride, padding="same")(x)
+    x = bn_relu(x, relu="relu6")
+    x = DepthwiseConv2D(kernel_size=3, strides=1, padding="same")(x)
     x = bn_relu(x, relu=None)
     x = Conv2D(out_channels, kernel_size=1, padding="same")(x)
-    x = bn_relu(x, relu="relu")
+    x = bn_relu(x, relu="relu6")
     return x
 
 
@@ -68,35 +71,47 @@ def shuffle_block(inputs, out_channels):
     return x
 
 
-def shuffle_block_down_sampling(inputs, out_channels):
-    x0 = DepthwiseConv2D(kernel_size=3, strides=2, padding="same")(inputs)
-    x0 = bn_relu(x0, relu=None)
-    x0 = Conv2D(out_channels, kernel_size=1, padding="same")(x0)
-    x0 = bn_relu(x0, relu="relu")
+# def shuffle_block_down_sampling(inputs, out_channels):
+#     x0 = DepthwiseConv2D(kernel_size=3, strides=2, padding="same")(inputs)
+#     x0 = bn_relu(x0, relu=None)
+#     x0 = Conv2D(out_channels, kernel_size=1, padding="same")(x0)
+#     x0 = bn_relu(x0, relu="relu")
 
-    x = Conv_DWConv_Conv(inputs, out_channels, stride=2)
+#     x = Conv_DWConv_Conv(inputs, out_channels, stride=2)
 
-    out = Concatenate()([x0, x])
-    out = channel_shuffle(out)
-    return out
+#     out = Concatenate()([x0, x])
+#     out = channel_shuffle(out)
+#     return out
+
+
+# def shuffle_unit(inputs, outputs=128):
+#     x = Conv2D(outputs, kernel_size=3, strides=1, padding="same")(inputs)
+#     x = bn_relu(x)
+#     x = Conv2D(outputs, kernel_size=3, strides=1, padding="same")(x)
+#     x = bn_relu(x, relu=None)
+#     x = shuffle_block(x, outputs)
+
+#     x = Concatenate(axis=-1)([x, inputs])
+#     x = ReLU()(x)
+#     return x
 
 
 def block1(inputs):
-    x = Conv2D(32, kernel_size=3, strides=2, padding="same")(inputs)
+    x = Conv2D(block1_conv_filter, kernel_size=3, strides=2, padding="same")(inputs)
     x = bn_relu(x, relu="relu")
-    x = Conv1D(32, kernel_size=3, strides=1, padding="same")(x)
+    x = Conv1D(block1_conv_filter, kernel_size=3, strides=1, padding="same")(x)
     x = bn_relu(x, relu="relu")
-    x = shuffle_block(x, 32)
+    x = shuffle_block(x, block1_conv_filter)
     return x
 
 
 def block2(inputs):
     # inv-bottle, 128x1x1
-    x = Conv2D(128, kernel_size=1, strides=1, padding="same")(inputs)
+    x = Conv2D(block2_conv_filter, kernel_size=1, strides=1, padding="same")(inputs)
     x = bn_relu(x, relu="relu6")
     x = MaxPooling2D((1, 1))(x)
     x = bn_relu(x, relu="relu6")
-    x = shuffle_block_down_sampling(x, 128)
+    x = shuffle_block(x, block2_conv_filter)
     return x
 
 
@@ -109,25 +124,21 @@ def GAP_classification(inputs, classes=100):
     ])
     x = tf.reduce_mean(inputs, axis=[1, 2])
     x = Dropout(0.5)(x)
-    x = Dense(classes, activation="sigmoid", name='out')(x)
-    print("[OUTPUT] shape:", x.shape.as_list())
+    x = Dense(
+        classes,
+        activation="sigmoid",
+        kernel_initializer=tf.random_normal_initializer(stddev=.01),
+        name='out'
+    )(x)
     return x
 
 
 def TVN(inputs, out_chanels, vstack=True):
-    global batch_size
-
-    repeat = 3
+    repeat = 4
     block1_repeat = repeat
     block2_repeat = repeat
 
     bs, h, w, c = inputs.shape.as_list()
-    batch_size = bs
-    # num_frames = h//240
-
-    print(inputs.shape.as_list())
-    print("batch_size", batch_size)
-
     x = inputs
 
     if vstack:
@@ -146,7 +157,9 @@ def TVN(inputs, out_chanels, vstack=True):
 
 if __name__ == '__main__':
     # for testing
+    batch_size = 32
     num_frames = 2
+
     inputs = Input(shape=(num_frames*240, 320, 1), batch_size=batch_size)
     model = TVN(inputs, 100)
     model.summary()
